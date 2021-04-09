900a7646fb7f9e9c3d9a821dfb80067b7f045d54
johanqsel <johanqsel@gmail.com>
2021-04-09 21:44:48 +0200

    changement du 09/04/21

--- contact2.js
+++ contact2.js
@@ -2,7 +2,7 @@ window.addEventListener('load', animer);
 
 let ang = 0;
 function animer(){
-    let t = setTimeout('animer()', 50);
+    let t = setTimeout('animer()', 30);
     let elem = document.getElementById("circle1");
     ang += 15;
     if (ang<3600){
